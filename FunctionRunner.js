500d12f2b369fae86a9e429a10e6d461a2d08054
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2023-07-11 19:40:47 +0200

    pure functions

--- FunctionRunner.js
+++ FunctionRunner.js
@@ -13,26 +13,35 @@ export class FunctionRunner {
 
     step() {
         if (this.currentNode instanceof FunctionCall) {
-            if (this.currentNode.fun instanceof NativeFunction) {
-                const inputs = Object.fromEntries(this.fun.connections.filter(x => x.to[0] == this.currentNode.id).map(x => {
-                    let value = null;
-                    const source = this.fun.elements.find(e => e.id === x.from[0]);
-                    if (source instanceof ConstantValue) {
-                        value = source.value;
-                    } else if (source instanceof FunctionCall) {
-                        value = this.functionResponses[source.id][x.from[1]];
-                    }
+            this.functionResponses[this.currentNode.id] = this.executeFunction(this.currentNode)
 
-                    return [x.to[1], value]
-                }));
-                this.functionResponses[this.currentNode.id] = this.currentNode.fun.execute(inputs);
-            }
         }
         const connection = this.fun.connections.find(x => x.from[0] == this.currentNode.id && x.from[1] == '__exit')
         if (connection) {
             this.currentNode = this.fun.elements.find(x => x.id === connection.to[0]);
         } else {
             return {}
+        }
+    }
+
+    executeFunction(node) {
+        if (node.fun instanceof NativeFunction) {
+            const inputs = Object.fromEntries(this.fun.connections.filter(x => x.to[0] == node.id).map(x => {
+                let value = null;
+                const source = this.fun.elements.find(e => e.id === x.from[0]);
+                if (source instanceof ConstantValue) {
+                    value = source.value;
+                } else if (source instanceof FunctionCall) {
+                    if (source.fun.pure) {
+                        value = this.executeFunction(source)[x.from[1]];
+                    } else {
+                        value = this.functionResponses[source.id][x.from[1]];
+                    }
+                }
+
+                return [x.to[1], value]
+            }));
+            return node.fun.execute(inputs);
         }
     }
 }
