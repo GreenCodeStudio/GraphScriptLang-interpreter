b9f3471746830ceb511672d2fa78c435841d8cbd
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2023-07-11 19:24:11 +0200

    css

--- FunctionRunner.js
+++ FunctionRunner.js
@@ -1,10 +1,12 @@
 import {Input} from "@graph-script-lang/core/Graphs/Input";
 import {FunctionCall} from "@graph-script-lang/core/Graphs/FunctionCall";
 import {NativeFunction} from "@graph-script-lang/core/NativeFunction";
+import {ConstantValue} from "@graph-script-lang/core/Graphs/ConstantValue";
 
 export class FunctionRunner {
     constructor(state, fun) {
         this.state = state;
+        this.functionResponses = {};
         this.fun = fun;
         this.currentNode = fun.elements.find(x => x instanceof Input);
     }
@@ -12,10 +14,25 @@ export class FunctionRunner {
     step() {
         if (this.currentNode instanceof FunctionCall) {
             if (this.currentNode.fun instanceof NativeFunction) {
-                this.currentNode.fun.execute(this);
+                const inputs = Object.fromEntries(this.fun.connections.filter(x => x.to[0] == this.currentNode.id).map(x => {
+                    let value = null;
+                    const source = this.fun.elements.find(e => e.id === x.from[0]);
+                    if (source instanceof ConstantValue) {
+                        value = source.value;
+                    } else if (source instanceof FunctionCall) {
+                        value = this.functionResponses[source.id][x.from[1]];
+                    }
+
+                    return [x.to[1], value]
+                }));
+                this.functionResponses[this.currentNode.id] = this.currentNode.fun.execute(inputs);
             }
         }
         const connection = this.fun.connections.find(x => x.from[0] == this.currentNode.id && x.from[1] == '__exit')
-        this.currentNode = this.fun.elements.find(x => x.id === connection.to[0]);
+        if (connection) {
+            this.currentNode = this.fun.elements.find(x => x.id === connection.to[0]);
+        } else {
+            return {}
+        }
     }
 }
--- RuntimeState.js
+++ RuntimeState.js
@@ -1,13 +1,17 @@
 import {ClassCollection} from "@graph-script-lang/core/ClassCollection";
 import {FunctionRunner} from "./FunctionRunner";
 import {GSLDebug} from "@graph-script-lang/stdlib/Debug/debug";
+import {GSLMath} from "@graph-script-lang/stdlib/Math/math";
+import {GSLConvert} from "@graph-script-lang/stdlib/Convert/convert";
 
 export class RuntimeState {
     stack = [];
 
     constructor() {
         this.classCollection = new ClassCollection();
-        this.classCollection.classes['GSL/Debug'] = GSLDebug;
+        this.classCollection.add(GSLDebug)
+        this.classCollection.add(GSLMath)
+        this.classCollection.add(GSLConvert)
 
 
     }
